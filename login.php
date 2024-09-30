2afc1deebf86e79490fe38b27ab7f20b5c747245
krlos-89 <krlos.evil@gmail.com>
2024-09-30 02:14:53 -0500

    creacion login y registro

--- login.php
+++ login.php
@@ -1,21 +1,74 @@
 <?php session_start();
 
-//Hacemos la conexion a la base de datos
+// Hacemos la conexión a la base de datos
 require 'conexion/conexion.php';
+$errores = 0;
 
 if (isset($_SESSION['usuarios'])) {
-    // $email = $_SESSION['usuarios'];
-    // $consultarROl = $conexion->prepare('SELECT roles_id FROM usuarios WHERE correo = :correo');
-    // $consultarROl->execute(array(':correo' => $email));
-    // $resultadoConsulta = $consultarROl->fetch();
+    $email = $_SESSION['usuarios'];
+    $consultarROl = $conexion->prepare('SELECT id_roles FROM usuarios WHERE Correo_Electronico = :correo');
+    $consultarROl->execute(array(':correo' => $email));
+    $resultadoConsulta = $consultarROl->fetch();
 
-    // if($resultadoConsulta['roles_id'] == 2){ // El cliente
-    //     header('Location: dashboard.php');
-    // } else if($resultadoConsulta['roles_id'] == 1){ // Si es administrador
-    //     header('Location: admin/dashboard.php');
-    // }
-} else {
-    header('Location: views/login.view.php');
+    print_r($resultadoConsulta);
+
+    if ($resultadoConsulta['id_roles'] == 2) { // El cliente
+        header('Location: dashboard.php');
+        exit();
+    } else if ($resultadoConsulta['id_roles'] == 1) { // Si es administrador
+        header('Location: admin/dashboard.php');
+        exit();
+    }
 }
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $Correo = isset($_POST['usuario']) ? $_POST['usuario'] : null;
+    $Contrasena = isset($_POST['Contrasena']) ? $_POST['Contrasena'] : null;
+
+    // Imprimir los datos para depuración
+    print_r($_POST);
+    print($Correo);
+    print($Contrasena);
+
+    if (empty($Correo) || empty($Contrasena)) {
+        echo "vacio";
+    } 
+    
+   
+    
+    else {
+        $q = $conexion->prepare("SELECT * FROM usuarios WHERE Correo_Electronico = :correo AND Contrasena = :contrasena");
+        $q->execute(array(':correo' => $Correo, ':contrasena' => $Contrasena));
+        $resultadoq = $q->fetchAll();
+
+        
+       
+
+        if (count($resultadoq) > 0) {
+            $_SESSION['Correo_electronico'] = $Correo;
+            $usuario = $resultadoq[0]; // Obtén el primer usuario
+
+           
+
+            if ($usuario['id_roles'] == 2) { // El cliente
+                header('Location: dashboard.php');
+                exit();
+            } else if ($usuario['id_roles'] == 1) { // Si es administrador
+                header('Location: admin/dashboard.php');
+                exit();
+            }
+        } 
+        
+           else {
+            echo '
+                <script>
+                    alert("Datos incorrectos");
+                    window.location = "signup.php";
+                </script>';
+            exit();
+        }
+    }
+}
+
+require "views/login.view.php";
 ?>
--- views/signup.view.php
+++ views/signup.view.php
@@ -6,10 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Restaurante las Delicias de la Tía Yú</title>
-    <link rel="shortcut icon" href="views/iconos/logo_1.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="iconos/logo_1.ico" type="image/x-icon">
     <script src="https://kit.fontawesome.com/9836403ffa.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="views/Estilos/style.css">
-    <link rel="stylesheet" href="views/Estilos/login.css">
+    <link rel="stylesheet" href="Estilos/style.css">
+    <link rel="stylesheet" href="Estilos/login.css">
+    <link rel="stylesheet" href="Estilos/singup.css">
 
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
@@ -18,7 +19,7 @@
     <header>
         <div class="header__superior">
             <div class="logo">
-                <img src="views/iconos/LAS DELICIAS.png" alt="">
+                <img src="iconos/LAS DELICIAS.png" alt="">
             </div>
         </div>
 
@@ -43,14 +44,25 @@
     </header>
     <main>
 
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" class="form-login">
-    <h5>Formulario Login</h5>
-    <input class="controls" type="text" name="usuario" placeholder="Usuario" required>
-    <input class="controls" type="password" name="Contrasena" placeholder="Contraseña" required>
-    <input class="buttons" type="submit" value="Ingresar">
-    <p><a href="#">¿Olvidaste tu Contraseña?</a></p>
-    <p><a href="../signup.php">Registrarse</a></p>
-</form>
+        <form method="post" id="signup" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" class="form-login">
+            <h5>Registro</h5>
+            <input class="controls" type="number" name="cedula" value="" placeholder="Cedula" required>
+            <div class="box">
+                <input class="controls" type="text" name="nombre1" placeholder="Primer Nombre" required>
+                <input class="controls" type="text" name="nombre2" placeholder="Segundo Nombre">
+            </div>
+            <div class="box">
+                <input class="controls" type="text" name="apellido1" placeholder="Primer Apellido" required>
+                <input class="controls" type="text" name="apellido2" placeholder="Segundo Apellido">
+            </div>
+            <input class="controls" type="number" name="telefono" placeholder="Telefono" require>
+            <input class="controls" type="mail" name="correo" placeholder="Correo" require>
+            <input class="controls" type="password" name="password" placeholder="Contraseña" required>
+
+            <input class="buttons" type="submit" name="" value="Registrarse">
+            <p><a href="#">¿Olvidastes tu Contraseña?</a></p>
+            <p><a href="../login.php">Ingresar</a></p>
+        </form>
 
     </main>
 
@@ -59,7 +71,7 @@
         <div class="container_footer_all">
             <div class="container_body">
                 <div class="columna">
-                    <img src="views/iconos/LAS DELICIAS.png">
+                    <img src="iconos/LAS DELICIAS.png">
 
                     <h2>Horario de Atención</h2>
                     <p>Lunes a Viernes</p>
@@ -72,26 +84,26 @@
                     <h2>Información de Contacto</h2>
                     <div class=row2>
                         <a href="https://goo.gl/maps/R2zp29zb5QXfWvY59" target="_blank">
-                            <img src="views/iconos/icons8-edificio-de-restaurante-50.png">
+                            <img src="iconos/icons8-edificio-de-restaurante-50.png">
                             <label>Mz B Casa 28 Barrio El Futuro Espinal, Tolima.
                             </label>
                         </a>
                     </div>
                     <div class=row2>
                         <a href="https://api.whatsapp.com/send?phone=573214172645&text=¿Cuál es el Menú para el Día de Hoy?" target="_blank">
-                            <img src="views/iconos/whatsapp30.png" href="">
+                            <img src="iconos/whatsapp30.png" href="">
                             <label>3214172645</label>
                         </a>
                     </div>
                     <div class=row2>
                         <a href="tel:573178338178">
-                            <img src="views/iconos/icons8-teléfono-desconectado-50.png">
+                            <img src="iconos/icons8-teléfono-desconectado-50.png">
                             <label>3178338178</label>
                         </a>
                     </div>
                     <div class=row2>
                         <a href="mailto:lasdeliciasdelatiayu@gmail.com" target="_blank">
-                            <img src="views/iconos/msj.png">
+                            <img src="iconos/msj.png">
                             <label>lasdeliciasdelatiayu@gmail.com</label>
                         </a>
                     </div>
@@ -100,13 +112,13 @@
                     <h2>Nuestras Redes Sociales</h2>
                     <div class=row>
                         <a href="https://www.facebook.com/profile.php?id=100087026563677" target="_blank">
-                            <img src="views/iconos/facebook50.png">
+                            <img src="iconos/facebook50.png">
                         </a>
                         <a href="https://www.instagram.com/lasdeliciasdelatiayu/" target="_blank">
-                            <img src="views/iconos/instagram30.png">
+                            <img src="iconos/instagram30.png">
                         </a>
                         <a href="https://pin.it/5DxHbnw" target="_blank">
-                            <img src="views/iconos/pinterest30.png">
+                            <img src="iconos/pinterest30.png">
                         </a>
                     </div>
                 </div>
